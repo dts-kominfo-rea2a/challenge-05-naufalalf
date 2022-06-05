31404600c0f4c0666c4c818a508715f42b6625af
naufalalfikri <naufalalfikri@gmail.com>
2022-06-05 19:15:51 +0700

    sudah dikerjakan tugas09

--- index.js
+++ index.js
@@ -6,15 +6,26 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (names, sort) => {
+  datSort = sort(names);
+  let arrHasil = [];
+  for(let i=0; i<names.length; i++){
+    arrHasil.push(i+1 +'.'+' '+names[i]);
+  }
+  return arrHasil;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (arrSort) => {
+    return arrSort.sort();
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (arrSort) => {
+  return arrSort.sort().reverse();
+};
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
